8fb3d0049ef5ac9c39ca17a4085677fd3ae51cf1
maj-alizadeh <majed.alizadeh@gmail.com>
2022-11-01 22:10:08 +0330

    add hover that acts as a pen

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ container.classList.add('container');
     for (let j = 0; j < 16; j++) {
         for (i = 0; i < 16; i++){
         let cell = document.createElement('div');
-        cell.innerText= '.'
         container.appendChild(cell) ;
         }
     }
 document.body.appendChild(container);
+container.addEventListener('mouseover', hover)
+
+function hover(e) {
+    e.stopPropagation();
+    e.target.classList.add('hover');
+    
+}
