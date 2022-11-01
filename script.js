568e879afdbd77c62bec3cc1a501b4f7bdd87a0b
maj-alizadeh <majed.alizadeh@gmail.com>
2022-11-01 23:40:21 +0330

    Add grid size button

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-let container = document.createElement('div')
-container.classList.add('container');
-    for (let j = 0; j < 16; j++) {
-        for (i = 0; i < 16; i++){
+let size = 16;
+let container = document.createElement('div');
+container.classList.add('page');
+let gridButton = document.createElement('button');
+gridButton.classList.add('grid-button')
+gridButton.textContent= 'Grid Size';
+document.body.appendChild(gridButton);
+gridButton.addEventListener('click', gridSize)
+
+function gridSize() {
+    size = prompt('Please enter the grid size:')
+    container.innerHTML='';
+    //page.style.gridTemplateColumns = `repeat (${size},1fr)`;
+    sketch();
+
+}
+// creates the grid
+function sketch() {
+    container.setAttribute('style',`grid-template-columns:repeat(${size},1fr)`)
+    for (let j = 0; j < size; j++) {
+        for (i = 0; i < size; i++){
         let cell = document.createElement('div');
         container.appendChild(cell) ;
         }
     }
-document.body.appendChild(container);
-container.addEventListener('mouseover', hover)
-
-function hover(e) {
-    e.stopPropagation();
-    e.target.classList.add('hover');
-    
 }
+document.body.appendChild(container);
+    
+    // on hover acts as a pen!
+    function hover(e) {
+        e.stopPropagation();
+        e.target.classList.add('hover');
+    }
+
+sketch();
+container.addEventListener('mouseover', hover)
